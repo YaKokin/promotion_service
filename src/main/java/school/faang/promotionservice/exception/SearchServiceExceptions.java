25ab702ad92197930ec2102e047c34c5684a14aa
Nikolay Orlenko <norlenko2801@gmail.com>
2024-12-18 23:09:47 +0300

    начал рефакторинг фильтров

--- src/main/java/school/faang/promotionservice/exception/SearchServiceExceptions.java
+++ src/main/java/school/faang/promotionservice/exception/SearchServiceExceptions.java
@@ -4,4 +4,8 @@ public class SearchServiceExceptions extends RuntimeException {
     public SearchServiceExceptions(String message, Throwable cause) {
         super(message, cause);
     }
+
+    public SearchServiceExceptions(Throwable cause) {
+        super(cause);
+    }
 }
---
+++ src/main/java/school/faang/promotionservice/model/search/PromotionUserDocFields.java
@@ -0,0 +1,9 @@
+package school.faang.promotionservice.model.search;
+
+public final class PromotionUserDocFields {
+
+    private static final String EXPERIENCE_FIELD = "experience";
+    private static final String USER_ID_FIELD = "userId";
+
+    private PromotionUserDocFields() {}
+}
